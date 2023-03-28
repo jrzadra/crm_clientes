79e56c6542b44bcdd66fb364ebc16ed95412b31e
jrzadra <jrzadra@gmail.com>
2023-03-28 14:45:53 -0300

    Alteracao do codigo, foi acrescentado mais atributos e metodos,codigo ainda precisa ser completado

--- src/main/java/com/ultimaschool/java/clientes/Cliente.java
+++ src/main/java/com/ultimaschool/java/clientes/Cliente.java
@@ -2,20 +2,76 @@ package com.ultimaschool.java.clientes;
 
 public class Cliente {
     
+    private String nomeCompleto;
+
+    private String primeiroNome;
+
+    private String nomeDoMeio;
+
+    private String sobrenome;
+
     private String cpf;
+
+    private String dataDeNascimento;
+
+    private int idadeAtual;
+
+    private char genero;
 
     private String email;
 
-    private String nomeCompleto;
+    private String endereco;
 
-    private char genero;
+    private String telefone;
+
     
-
-    public Cliente(String cpf, String email, String nomeCompleto, char genero) {
-        this.cpf = cpf;
-        this.email = email;
+    public Cliente(String nomeCompleto, String primeiroNome, String nomeDoMeio, String sobrenome,
+     String cpf, String dataDeNascimento, int idadeAtual, char genero, String email, 
+     String endereco, String telefone) {
         this.nomeCompleto = nomeCompleto;
+        this.primeiroNome = primeiroNome;
+        this.nomeDoMeio = nomeDoMeio;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.dataDeNascimento = dataDeNascimento;
+        this.idadeAtual = idadeAtual;
         this.genero = genero;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getNomeDoMeio() {
+        return nomeDoMeio;
+    }
+
+    public void setNomeDoMeio(String nomeDoMeio) {
+        this.nomeDoMeio = nomeDoMeio;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
     }
 
     public String getCpf() {
@@ -26,20 +82,20 @@ public class Cliente {
         this.cpf = novoCpf;
     }
 
-    public String getEmail() {
-        return email;
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
     }
 
-    public String getNomeCompleto() {
-        return nomeCompleto;
+    public int getIdadeAtual() {
+        return idadeAtual;
     }
 
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
+    public void setIdadeAtual(int idadeAtual) {
+        this.idadeAtual = idadeAtual;
     }
 
     public char getGenero() {
@@ -59,6 +115,31 @@ public class Cliente {
             return "";
         }
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
 
     @Override
     public String toString() {
