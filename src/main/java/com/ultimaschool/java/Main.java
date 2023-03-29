17b3dc7008f80ad26abfab6521378dc701d16526
jrzadra <jrzadra@gmail.com>
2023-03-29 20:17:32 -0300

    Criacao de classe Cliente usando padrao de projeto Builder

--- src/main/java/com/ultimaschool/java/Main.java
+++ src/main/java/com/ultimaschool/java/Main.java
@@ -1,6 +1,7 @@
 package com.ultimaschool.java;
 
 import com.ultimaschool.java.clientes.Cliente;
+import com.ultimaschool.java.clientes.ClienteBuilder;
 
 public class Main {
     public static void main( String[] args ) {
@@ -10,7 +11,6 @@ public class Main {
         'F', "mariadorocio78@gmail.com",
         "Rua Manoel Gallardo, nº 134, Labra - São Paulo",
         "(11) 99999-0000");
-       
         System.out.println(clienteF.toString() + "\n");
         
         Cliente clienteM = new Cliente("Danilo", "",
@@ -18,6 +18,24 @@ public class Main {
         'M', "dangraciotto@outlook.com", 
         "Rua Augusta, nº 539, Centro - São Paulo",
         "(11) 11111-2222");
-        System.out.println(clienteM.toString());
+        System.out.println(clienteM.toString() + "\n");
+
+        ClienteBuilder clienteFCompleto = new ClienteBuilder().comIdentificacao("Maria",
+        "do Rocio", "Conceição", "123.456.789-00",
+        "12/04/1978", 'F').comContatos("mariadorocio78@gmail.com",
+        "Rua Manoel Gallardo, nº 134, Labra - São Paulo",
+        "(11) 99999-0000");
+        System.out.println(clienteFCompleto.toString() + "\n");
+
+        ClienteBuilder clienteMContatos = new ClienteBuilder().comContatos(
+        "dangraciotto@outlook.com", 
+        "Rua Augusta, nº 539, Centro - São Paulo",
+        "(11) 11111-2222");
+        System.out.println(clienteMContatos.toStringContatos() + "\n");
+
+        ClienteBuilder clienteMIdentificacao = new ClienteBuilder().comIdentificacao(
+        "Danilo", "", "Graciotto", "098.765.432-10",
+        "22/07/1997", 'M');
+        System.out.println(clienteMIdentificacao.toStringIdentificacao() + "\n");
     }
 }
